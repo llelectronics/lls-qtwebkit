4f30d05c433cfb567d2ce71cda6c8a660dc2f93a
Leszek Lesner <leszek.lsner@web.de>
2016-05-18 20:30:07 +0200

    Fix crash on exit in OpenGL accelerated mode

--- qtwebkit/Source/WebCore/platform/graphics/qt/GraphicsContext3DQt.cpp
+++ qtwebkit/Source/WebCore/platform/graphics/qt/GraphicsContext3DQt.cpp
@@ -459,22 +459,23 @@ GraphicsContext3D::~GraphicsContext3D()
     if (!m_private)
         return;
 
-    makeContextCurrent();
-    glDeleteTextures(1, &m_texture);
-    glDeleteFramebuffers(1, &m_fbo);
-    if (m_attrs.antialias) {
-        glDeleteRenderbuffers(1, &m_multisampleColorBuffer);
-        glDeleteFramebuffers(1, &m_multisampleFBO);
-        if (m_attrs.stencil || m_attrs.depth)
-            glDeleteRenderbuffers(1, &m_multisampleDepthStencilBuffer);
-    } else if (m_attrs.stencil || m_attrs.depth) {
+    if (makeContextCurrent()) {
+      glDeleteTextures(1, &m_texture);
+      glDeleteFramebuffers(1, &m_fbo);
+      if (m_attrs.antialias) {
+          glDeleteRenderbuffers(1, &m_multisampleColorBuffer);
+          glDeleteFramebuffers(1, &m_multisampleFBO);
+          if (m_attrs.stencil || m_attrs.depth)
+              glDeleteRenderbuffers(1, &m_multisampleDepthStencilBuffer);
+      } else if (m_attrs.stencil || m_attrs.depth) {
 #if USE(OPENGL_ES_2)
-        if (m_attrs.depth)
-            glDeleteRenderbuffers(1, &m_depthBuffer);
-        if (m_attrs.stencil)
-            glDeleteRenderbuffers(1, &m_stencilBuffer);
+          if (m_attrs.depth)
+              glDeleteRenderbuffers(1, &m_depthBuffer);
+          if (m_attrs.stencil)
+              glDeleteRenderbuffers(1, &m_stencilBuffer);
 #endif
-        glDeleteRenderbuffers(1, &m_depthStencilBuffer);
+          glDeleteRenderbuffers(1, &m_depthStencilBuffer);
+      }
     }
 }
 
