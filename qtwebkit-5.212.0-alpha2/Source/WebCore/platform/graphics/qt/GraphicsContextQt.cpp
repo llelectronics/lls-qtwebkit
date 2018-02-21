3b54defa0a2326ceaf154d28e7c49e8f0f6b070d
Leszek Lesner <leszek.lesner@web.de>
2018-02-21 11:48:42 +0100

    Fix Crash on npmjs.com

--- qtwebkit-5.212.0-alpha2/Source/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
+++ qtwebkit-5.212.0-alpha2/Source/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
@@ -1343,6 +1343,8 @@ void GraphicsContext::endPlatformTransparencyLayer()
     while ( ! m_data->layers.top()->alphaMask.isNull() ){
         --m_data->layers.top()->saveCounter;
         popTransparencyLayerInternal();
+        if (m_data->layers.isEmpty())
+            return;
     }
     TransparencyLayer* layer = m_data->layers.pop();
     --m_data->layerCount; // see the comment for layerCount
