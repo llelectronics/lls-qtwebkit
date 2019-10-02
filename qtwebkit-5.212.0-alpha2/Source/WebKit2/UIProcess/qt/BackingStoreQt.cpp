ebc861b5bfd85139a8ed7485127e4f24a70cf69b
Leszek Lesner <leszek.lesner@web.de>
2019-10-02 21:56:44 +0200

    Correctly create pointer

--- qtwebkit-5.212.0-alpha2/Source/WebKit2/UIProcess/qt/BackingStoreQt.cpp
+++ qtwebkit-5.212.0-alpha2/Source/WebKit2/UIProcess/qt/BackingStoreQt.cpp
@@ -40,9 +40,9 @@ namespace WebKit {
 void BackingStore::paint(QPainter* painter, const IntRect& rect)
 {
     ASSERT(!m_pixmap.isNull());
-    IntRect& llrect = rect;
-    llrect.setHeight(rect.height() * 2);
-    painter->drawPixmap(llrect, m_pixmap, llrect);
+    IntRect *llrect = rect;
+    llrect->setHeight(rect.height() * 2);
+    painter->drawPixmap(&llrect, m_pixmap, &llrect);
 }
 
 void BackingStore::incorporateUpdate(ShareableBitmap* bitmap, const UpdateInfo& updateInfo)
